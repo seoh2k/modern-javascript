d937feef0c6f9483ab6bcf88b8cd007309fe9b2d
kseoh2 <seoh2k@gmail.com>
2022-12-04 01:02:23 +0900

    first commit

---
+++ esnext-project/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    // entry file
+    entry: ['@babel/polyfill', './src/js/main.js'],
+
+    // 번들링된 js 파일의 이름(filename)과 저장될 경로(path)를 지정
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, 'src/js')
+                ],
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    mode: 'development'
+}
